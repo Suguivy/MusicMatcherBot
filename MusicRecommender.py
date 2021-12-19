34582847bd17712878273428e8e22d4741422cf6
Suguivy <adrisolgop@gmail.com>
2021-12-19 19:02:44 +0100

    Ahora el bot funciona bastante bien, poco queda para mejorar

--- MusicRecommender.py
+++ MusicRecommender.py
@@ -8,117 +8,121 @@ from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import pairwise_distances
 
-# Converts the CSV data into a dataframe
-originalData = pd.read_csv('spotify_songs.csv')
+class MusicRecommender:
+    def __init__(self):
+      self.dataset = None
+      self.distance_matrix = None
 
-# We get rid of the songs that are not in English, to make correct predictions based in the lyrics
-englishSongs = originalData.query('language == "en"')
-englishSongs = englishSongs.reset_index()
-englishSongs['english_index'] = range(0,len(englishSongs))
+    def load_all_from_csv(self, csv_file, dataset_output, matrix_output):
+        if (not os.path.exists(dataset_output)):
+            self.load_dataset_from_csv(csv_file, save_file=dataset_output)
+        else:
+            self.load_dataset_from_file(dataset_output)
+        if (not os.path.exists(matrix_output)):
+            self.calculate_matrix_from_dataset(matrix_output)
+        else:
+            self.load_matrix_from_file(matrix_output)
 
-useCachedData = True
+    def load_dataset_from_file(self, filename):
+        print(f"Loading dataset '{filename}'...")
 
-preprocessedData = None
-if not useCachedData :
-  nltk.download('punkt')
-  nltk.download('stopwords')
-  ps = PorterStemmer()
-  preprocessedText = []
-  i = 0
-  for row in englishSongs.itertuples():
-    text = word_tokenize(row.lyrics) ## indice de la columna que contiene el texto
-    ## Remove stop words
-    stops = set(stopwords.words("english"))
-    text = [ps.stem(w) for w in text if not w in stops and w.isalnum()]
-    text = " ".join(text)
+        if os.path.exists(filename):
+            self.dataset = pd.read_pickle(filename)
+        else:
+            raise Exception('dataset file not found')
 
-    preprocessedText.append(text)
-    if i % 100 == 0:
-      print(i)
-    i += 1
+    def load_dataset_from_csv(self, csv_file, save_file='dataset'):
+        """
+        Creates and loads the distance matrix from the CSV file
+        """
+        print(f"Loading and converting '{csv_file}' into a dataset...")
 
-  preprocessedData = englishSongs
-  preprocessedData['processed_text'] = preprocessedText
-else:
-  preprocessedData = pd.read_pickle("data")
+        df = pd.read_csv(csv_file)
+        df = df.query("language == 'en'") # We only want songs in english to get appropiate words to work with
+        df = df.reset_index()
+        df['english_index'] = range(0, len(df))
 
-bagOfWordsModel = TfidfVectorizer()
-bagOfWordsModel.fit(preprocessedData['processed_text'])
-textsBoW= bagOfWordsModel.transform(preprocessedData['processed_text'])
+        # Tokenization of words
+        nltk.download('punkt', download_dir='./nltk_data')
+        nltk.download('stopwords', download_dir='./nltk_data')
+        nltk.data.path.append('./nltk_data')
+        ps = PorterStemmer()
+        preprocessed_text = []
+        i = 0
+        for row in df.itertuples():
+            tokenized_words = word_tokenize(row.lyrics)
+            stops = set(stopwords.words("english"))
+            words = [ps.stem(word) for word in tokenized_words if not word in stops and word.isalnum()]
+            words = " ".join(words)
+            preprocessed_text.append(words)
 
-def getLyrics(title,dataset):
-  songIndex = dataset[dataset['track_name']==title].index.values[0]
-  return dataset.loc[songIndex]['lyrics']
+        self.dataset = df
+        self.dataset['processed_text'] = preprocessed_text
 
-def getSong(title,dataset):
-  songIndex = dataset[dataset['track_name']==title].index.values[0]
-  return dataset.loc[songIndex]
+        print(f"Saving dataset as '{save_file}'")
+        self.dataset.to_pickle(save_file)
 
-def getSongByIndex(idx,dataset):
-  return dataset.loc[idx]
+    def _get_column_values(self, column_name):
+        return self.dataset[column_name].to_numpy().reshape(-1,1)
 
-def getColumnValues(column_name,dataset):
-  return dataset[column_name].to_numpy().reshape(-1,1)
+    def _make_column_conf(self, column_name,metric,weight):
+        return { 'value': self._get_column_values(column_name), 'metric': metric, 'weight': weight}
 
-def makeColumnConf(column_name,metric,weight):
-  return { 'value': getColumnValues(column_name,preprocessedData), 'metric': metric, 'weight': weight}
+    def calculate_matrix_from_dataset(self, save_file='distance_matrix.npy'):
+        print("Calculating matrix from dataset...")
 
-configuration = {
-    "lyrics": { 'value': textsBoW
-               , 'metric': 'cosine'
-               , 'weight': 1},
-    'valences': makeColumnConf('valence','l1',20),
-    #'loudness': { 'value': getColumnValues('loudness',preprocessedData), 'metric': 'l1', 'weight': 20},
-    'tempo': makeColumnConf('tempo','l1',20),
-    'danceability': makeColumnConf('danceability','l1',20),
-    'energy': makeColumnConf('energy','l1',20),
-    'speechiness': makeColumnConf('speechiness','l1',20),
-    'acousticness': makeColumnConf('acousticness','l1',20),
-    'instrumentalness': makeColumnConf('instrumentalness','l1',20),
-    #'key' : makeColumnConf('key','l1',20),
-    #'mode' : makeColumnConf('mode','l1',20),
-    }
+        # Creation of the bag of words
+        bow_model = TfidfVectorizer()
+        bow_model.fit(self.dataset['processed_text'])
+        texts_bow = bow_model.transform(self.dataset['processed_text'])
 
-useCachedMatrix = True
-def getDistanceMatrix(configuration):
-  if useCachedMatrix and os.path.exists("distanceMatrix.npy"):
-    m = np.load("distanceMatrix.npy")
-    return m
-  else:
-    accum = None
-    total = 0
-    for k,v in configuration.items():
-      d = pairwise_distances(v['value'],v['value'],metric=v['metric'])*v['weight']
-      if accum is None:
-        accum = d
-      else:
-        accum = accum + d
-      total = total + v['weight']
-    m = accum/total
-    np.save("distanceMatrix.npy", m)
-    return m
+        configuration = {
+            'lyrics': { 'value': texts_bow,
+                        'metric': 'cosine',
+                        'weight': 1
+                      },
+            'valences': self._make_column_conf('valence','l1',20),
+            #'loudness': { 'value': getColumnValues('loudness',preprocessedData), 'metric': 'l1', 'weight': 20},
+            'tempo': self._make_column_conf('tempo','l1',20),
+            'danceability': self._make_column_conf('danceability','l1',20),
+            'energy': self._make_column_conf('energy','l1',20),
+            'speechiness': self._make_column_conf('speechiness','l1',20),
+            'acousticness': self._make_column_conf('acousticness','l1',20),
+            'instrumentalness': self._make_column_conf('instrumentalness','l1',20),
+            #'key' : self._make_column_conf('key','l1',20),
+            #'mode' : self._make_column_conf('mode','l1',20),
+        }
 
-def similar(searchTitle,distanceMatrix,dataset):
-   songIndex = preprocessedData[dataset['track_name']==searchTitle].index.values[0]
-   distance_scores = list(enumerate(distanceMatrix[songIndex]))
-   ordered_scores = sorted(distance_scores, key=lambda x: x[1])
-   top_scores = ordered_scores[1:11]
-   top_indexes = [i[0] for i in top_scores]
-   top_confidence = [i[1] for i in top_scores]
-   d = dataset['track_name'].iloc[top_indexes].to_frame()
+        # Calculation of the distance matrix
+        accum = None
+        total = 0
+        for k, v in configuration.items():
+            d = pairwise_distances(v['value'], v['value'], metric=v['metric']) * v['weight']
+            if accum is None:
+                accum = d
+            else:
+                accum = accum + d
+            total = total + v['weight']
+        self.distance_matrix = accum / total
 
-   d['confidence'] = pd.Series(top_confidence,index=top_indexes)
-   return d
+        print(f"saving matrix as '{save_file}")
+        np.save(save_file, self.distance_matrix)
 
-distanceConf = getDistanceMatrix(configuration)
+    def load_matrix_from_file(self, matrix_file):
+        print(f"Loading matrix '{matrix_file}'...")
+        if os.path.exists(matrix_file):
+            self.distance_matrix = np.load(matrix_file)
+        else:
+            raise Exception(f"distance matrix file '{matrix_file}' not found")
 
-#@title Sistema recomendador
-#@markdown Inserte el título de la canción para obtener canciones similares.
-searchTitle = "Op Opa"  #@param {type: "string"}
-#@markdown ---
-results = similar(searchTitle,distanceConf,preprocessedData)
-print(results.head(10))
-print(type(results))
+    def similar_by_exact_title(self, title, count=1):
+        song_index = self.dataset[self.dataset['track_name']==title].index.values[0]
+        distance_scores = list(enumerate(self.distance_matrix[song_index]))
+        ordered_scores = sorted(distance_scores, key=lambda x: x[1])
+        top_scores = ordered_scores[1:count+1]
+        top_indexes = [i[0] for i in top_scores]
+        top_confidence = [i[1] for i in top_scores]
+        d1 = self.dataset['track_name'].iloc[top_indexes].to_frame()
+        d2 = self.dataset['track_artist'].iloc[top_indexes].to_frame()
 
-song = getSong(searchTitle, preprocessedData)
-searchString = song['track_name'] + ' ' + song['track_artist']
+        return list(zip(d1['track_name'].tolist(), d2['track_artist'].tolist()))
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import discord
 import os
 import youtube_dl
+import asyncio
+import MusicRecommender as mr
 
 """
 Most of the code comes from https://python.land/build-discord-bot-in-python-that-plays-music
@@ -17,21 +19,67 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix="!")
-#bot = discord.Client()
+
+queue = []
+
+recommender = mr.MusicRecommender()
+recommender.load_all_from_csv('recommender_data/spotify_songs.csv', 'recommender_data/dataset', 'recommender_data/matrix.npy')
+
+def readable_queue():
+    if len(queue) == 0:
+        return "The queue is empty."
+    else:
+        return '\n'.join([f'{i} - _{v["title"]}_' for i, v in enumerate(queue)])
+
+def id_to_url(id):
+    return f"https://youtube.com/watch?v={id}"
+
+async def id_to_video(id):
+    result = await VideosSearch(video_title, limit = number_of_results).next()
+    return result['result'][0]
+
+async def search_by_title(title, num_of_results=1):
+    result = await VideosSearch(title, limit = num_of_results).next()
+    return result['result'][0]
+async def play(ctx):
+    pass
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
-@bot.command(name='search')
-async def search(ctx, video_title, number_of_results=1):
-    videosSearch = VideosSearch(video_title, limit = number_of_results)
-    videosResult = await videosSearch.next()
-    for v in videosResult['result']:
-        url = "https://youtube.com/watch?v="+v['id']
-        await ctx.channel.send(url)
+@bot.command(name='showqueue', aliases=['q'])
+async def _showqueue(ctx):
+    await ctx.send(readable_queue())
 
-@bot.command(name='join', help='Tells the bot to join the voice channel')
+@bot.command(name='create', aliases=['c'])
+async def _create(ctx, title, count=1):
+    similar = recommender.similar_by_exact_title(title, count)
+    similar_search_titles = [f'{song_title} {song_artist}' for song_title, song_artist in similar]
+    results = [await search_by_title(title) for title in similar_search_titles]
+    for video in results:
+        queue.append(video)
+
+@bot.command(name='play', aliases=['p'])
+async def _play(ctx):
+    while len(queue) > 0:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        if not (voice_channel and voice_channel.is_connected()):
+            await ctx.send('I\'m not connected to a voice channel, but I will connect to your current voice channel.')
+            await join(ctx)
+            server = ctx.message.guild
+            voice_channel = server.voice_client
+        video = queue.pop(0)
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(id_to_url(video['id']), loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=filename))
+        await ctx.send(f'**Playing:** {video["title"]}')
+        while voice_channel.is_playing():
+            await asyncio.sleep(.1)
+    await ctx.send("The queue is empty.")
+
+@bot.group(name='join', aliases=['j'])
 async def join(ctx):
     if not ctx.message.author.voice:
         await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
@@ -40,7 +88,7 @@ async def join(ctx):
         channel = ctx.message.author.voice.channel
         await channel.connect()
 
-@bot.command(name='leave', help='To make the bot leave the voice channel')
+@bot.command(name='leave', alisases=['l'])
 async def leave(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_connected():
@@ -48,19 +96,7 @@ async def leave(ctx):
     else:
         await ctx.send("The bot is not connected to a voice channel.")
 
-@bot.command(name='play', help='To play song')
-async def play(ctx,url):
-    try :
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-        async with ctx.typing():
-            filename = await YTDLSource.from_url(url, loop=bot.loop)
-            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=filename))
-        await ctx.send('**Now playing:** {}'.format(filename))
-    except:
-        await ctx.send("The bot is not connected to a voice channel.")
-
-@bot.command(name='pause', help='This command pauses the song')
+@bot.command(name='pause', aliases=['pp'])
 async def pause(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_playing():
@@ -68,15 +104,15 @@ async def pause(ctx):
     else:
         await ctx.send("The bot is not playing anything at the moment.")
 
-@bot.command(name='resume', help='Resumes the song')
+@bot.command(name='resume', aliases=['r'], help='Resumes the song')
 async def resume(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_paused():
         voice_client.resume()
     else:
-        await ctx.send("The bot was not playing anything before this. Use play_song command")
+        await ctx.send("I wasn't playing anything before this. Use `!play` or `!p`")
 
-@bot.command(name='stop', help='Stops the song')
+@bot.command(name='stop', aliases=['s'])
 async def stop(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_playing():
